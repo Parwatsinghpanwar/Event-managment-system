8372ce8d56b5e21ba6d297ad39bd4a16e79beb0d
parwatsinghpanwar <parwatpanwar8316@gmail.com>
2025-04-07 22:10:49 +0530

    add database fairbase api and online data collection

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiOvw0hYIzp3ziu00C7ErxC45hc2sBz4Q",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCdh5IiNIslWXC5fkhmE1as-lCIKKoykpI",
+  authDomain: "eventique-44a7c.firebaseapp.com",
+  projectId: "eventique-44a7c",
+  storageBucket: "eventique-44a7c.firebasestorage.app",
+  messagingSenderId: "758958277418",
+  appId: "1:758958277418:web:ede45b6005f8747362d453",
+  measurementId: "G-HF7W9XQQP5"
 };
 
 
